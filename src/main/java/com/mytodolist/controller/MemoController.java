7ba52a930dc91f537ce8c4f3a80136733f9f3f59
lkdcode <dlrlejr1@naver.com>
2023-04-22 22:46:57 +0900

    memo controller

---
+++ src/main/java/com/mytodolist/controller/MemoController.java
@@ -0,0 +1,13 @@
+package com.mytodolist.controller;
+
+import com.mytodolist.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+/**
+ * 메모 컨트롤러
+ */
+@Controller
+@RequiredArgsConstructor
+public class MemoController {
+}
