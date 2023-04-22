058305bcd3c3f536e0f4f678924c7006fb65710b
lkdcode <dlrlejr1@naver.com>
2023-04-22 22:46:19 +0900

    멤버 컨트롤러

---
+++ src/main/java/com/mytodolist/controller/MemberController.java
@@ -0,0 +1,12 @@
+package com.mytodolist.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+/**
+ * member 컨트롤러
+ */
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+}
